e778fd64c549d9764bf18f7ab4b73c7be1a4539a
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2023-07-25 12:05:13 -0300

    feat(users): implements prisma service and repository

---
+++ back/src/module/users/repositories/prisma/users.prisma.repository.ts
@@ -0,0 +1,56 @@
+import { PrismaService } from "prisma/prisma.service";
+import { CreateUserDto } from "../../dto/create-user.dto";
+import { UpdateUserDto } from "../../dto/update-user.dto";
+import { User } from "../../entities/user.entity";
+import { UsersRepository } from "../users.repository";
+import { Injectable } from "@nestjs/common";
+import { plainToInstance } from "class-transformer";
+
+@Injectable()
+export class UsersPrimasRepository implements UsersRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: CreateUserDto): Promise<User> {
+    const user = new User();
+    Object.assign(user, {
+      ...data,
+    });
+    const newUser = await this.prisma.user.create({
+      data: { ...user },
+    });
+    return plainToInstance(User, newUser);
+  }
+
+  async findAll(): Promise<User[]> {
+    const users: User[] = await this.prisma.user.findMany();
+    return plainToInstance(User, users);
+  }
+
+  async findOne(id: string): Promise<User> {
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+    });
+    return plainToInstance(User, user);
+  }
+
+  async findOneByEmail(email: string): Promise<User> {
+    const user = await this.prisma.user.findFirst({
+      where: { email },
+    });
+    return user;
+  }
+
+  async update(id: string, data: UpdateUserDto): Promise<User> {
+    const user = await this.prisma.user.update({
+      where: { id },
+      data: { ...data },
+    });
+    return plainToInstance(User, user);
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.prisma.user.delete({
+      where: { id },
+    });
+  }
+}
--- back/src/module/users/users.module.ts
+++ back/src/module/users/users.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import { Module } from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { UsersController } from "./users.controller";
+import { PrismaService } from "prisma/prisma.service";
+import { UsersPrimasRepository } from "./repositories/prisma/users.prisma.repository";
+import { UsersRepository } from "./repositories/users.repository";
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [
+    UsersService,
+    PrismaService,
+    { provide: UsersRepository, useClass: UsersPrimasRepository },
+  ],
 })
 export class UsersModule {}
