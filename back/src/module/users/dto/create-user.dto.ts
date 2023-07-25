90de125a7e1032a24a9d9252e47486b67309b4ee
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2023-07-25 11:48:46 -0300

    feat(main | users): implements validation pipes in global context | implements dto class

--- back/src/module/users/dto/create-user.dto.ts
+++ back/src/module/users/dto/create-user.dto.ts
@@ -1,1 +1,20 @@
-export class CreateUserDto {}
+import { hashSync } from "bcryptjs";
+import { Transform } from "class-transformer";
+import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(4)
+  @Transform(({ value }: { value: string }) => hashSync(value), {
+    groups: ["transform"],
+  })
+  password: string;
+}
