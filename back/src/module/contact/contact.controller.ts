79aee3f37cceb290def6e733167b3de718a2cbbe
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2023-07-26 09:07:44 -0300

    feat: standarize code style

--- back/src/module/contact/contact.controller.ts
+++ back/src/module/contact/contact.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { ContactService } from './contact.service';
-import { CreateContactDto } from './dto/create-contact.dto';
-import { UpdateContactDto } from './dto/update-contact.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from "@nestjs/common";
+import { ContactService } from "./contact.service";
+import { CreateContactDto } from "./dto/create-contact.dto";
+import { UpdateContactDto } from "./dto/update-contact.dto";
 
-@Controller('contact')
+@Controller("contact")
 export class ContactController {
   constructor(private readonly contactService: ContactService) {}
 
@@ -17,18 +25,18 @@ export class ContactController {
     return this.contactService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get(":id")
+  findOne(@Param("id") id: string) {
     return this.contactService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateContactDto: UpdateContactDto) {
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updateContactDto: UpdateContactDto) {
     return this.contactService.update(+id, updateContactDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @Delete(":id")
+  remove(@Param("id") id: string) {
     return this.contactService.remove(+id);
   }
 }
