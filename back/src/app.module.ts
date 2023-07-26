9b86462c5ad13861ccbe91e6b3825d271e733afe
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2023-07-26 08:46:31 -0300

    feat: add contact resource

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { UsersModule } from "./module/users/users.module";
 import { AuthModule } from "./module/auth/auth.module";
+import { ContactModule } from "./module/contact/contact.module";
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, ContactModule],
   controllers: [],
   providers: [],
 })
