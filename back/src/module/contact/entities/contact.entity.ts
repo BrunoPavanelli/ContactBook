b1bd8a0eb5045da3ea69846cea3a1f988121a817
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2023-07-26 09:27:46 -0300

    feat(contact): add entities

--- back/src/module/contact/entities/contact.entity.ts
+++ back/src/module/contact/entities/contact.entity.ts
@@ -1,1 +1,30 @@
-export class Contact {}
+import { randomUUID } from "node:crypto";
+
+export class Numbers {
+  readonly id: string;
+  number: string;
+
+  constructor() {
+    this.id = randomUUID();
+  }
+}
+
+export class Email {
+  readonly id: string;
+  email: string;
+
+  constructor() {
+    this.id = randomUUID();
+  }
+}
+
+export class Contact {
+  readonly id: string;
+  name: string;
+  numbers: string[];
+  emails: string[];
+
+  constructor() {
+    this.id = randomUUID();
+  }
+}
