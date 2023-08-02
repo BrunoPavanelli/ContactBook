49e562de1210bca7ad510b8345eb6ae6da61bddc
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2023-08-02 15:58:59 -0300

    feat(front): add header component initial configs

---
+++ front/src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import { HeaderDivStyled, HeaderStyled } from "./HeaderStyled";
+
+export const Header = () => {
+	return (
+		<HeaderStyled>
+			<HeaderDivStyled className="container__page">
+
+			</HeaderDivStyled>
+		</HeaderStyled>
+	);
+};
