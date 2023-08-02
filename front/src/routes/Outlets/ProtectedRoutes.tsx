db27f5b43db683189867a074581eaed74f90735f
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2023-08-02 15:56:24 -0300

    feat(front): add route system

---
+++ front/src/routes/Outlets/ProtectedRoutes.tsx
@@ -0,0 +1,9 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+export const ProtectedRoutes = () => {
+    const token = localStorage.getItem("@ContactBook:Token");
+
+    return token ? <Outlet /> : <Navigate to="/"/>;
+};
+
+
---
+++ front/src/routes/Routes.tsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+
+import { ProtectedRoutes } from "./Outlets/ProtectedRoutes";
+import { PublicRoutes } from "./Outlets/PublicRoutes";
+import { Login } from "../pages/Login/Login";
+import { Register } from "../pages/Register/Register";
+import { UserDash } from "../pages/UserDash/UserDash";
+
+export const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<PublicRoutes />}>
+                <Route index element={<Login />} />
+                <Route path="/register" element={<Register />} />
+            </Route>
+            <Route path="/dashboard" element={<ProtectedRoutes />}>
+                <Route index element={<UserDash />} />
+            </Route>
+        </Routes>
+    );
+};
