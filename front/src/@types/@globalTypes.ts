8c31582c467540a3eea66134ef9dac7c80036a11
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2023-08-02 15:59:41 -0300

    feat(front): add global types file

---
+++ front/src/@types/@globalTypes.ts
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+import { UseFormRegisterReturn } from "react-hook-form";
+
+export interface IChildren {
+    children: ReactNode;
+}
+
+export interface IChildrenOptional {
+    children?: ReactNode;
+}
+
+export interface IInput extends IChildren {
+    placeholder: string;
+    type: string;
+    register: UseFormRegisterReturn<string>;
+    errors?: string
+}
